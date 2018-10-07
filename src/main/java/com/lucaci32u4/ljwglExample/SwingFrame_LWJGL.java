ceb5c8d15adab1be3c3c751ecd741dd9ac2a8455
lucaci32u4 <lucaci32u4@gmail.com>
2018-10-07 16:55:29 +0300

    Slight code cleanup, modifications

--- src/main/java/com/lucaci32u4/ljwglExample/SwingFrame_LWJGL.java
+++ src/main/java/com/lucaci32u4/ljwglExample/SwingFrame_LWJGL.java
@@ -100,7 +100,6 @@ public class SwingFrame_LWJGL {
 		splitPane.setRightComponent(canvasPanel);
 		
 		canvas = new Canvas() {
-			private static final long serialVersionUID = -1069002023468669595L;
 			public void removeNotify() {
 				stopOpenGL();
 			}
@@ -148,23 +147,9 @@ public class SwingFrame_LWJGL {
 				try {
 					Display.create();
 					Display.setParent(canvas);
-					
-					Rectangle rect = canvas.getBounds();
-					int w = (int) rect.getWidth();
-					int h = (int) rect.getHeight();
-					
-					System.out.println("Display Rectangle: "+rect);
-					
-					GL11.glClearColor(0.1f, 0.1f, 0.3f, 1);
-					
-					GL11.glMatrixMode(GL11.GL_PROJECTION);
-					GL11.glLoadIdentity();
-					GL11.glOrtho(0, w, h, 0, -1, 1);
-					
-					GL11.glViewport(0, 0, w, h);
-					
+					GL11.glClearColor(1.0f, 1.0f, 1.0f, 1);
+					makesize();
 					setupVertices();
-					
 					running = true;
 				} catch (LWJGLException e) {
 					e.printStackTrace();
@@ -195,6 +180,17 @@ public class SwingFrame_LWJGL {
 		vertices[6] = 0.7f;
 		vertices[7] = 0.05f;
 	}
+
+	private void makesize() {
+		Rectangle rect = canvas.getBounds();
+		int w = (int) rect.getWidth();
+		int h = (int) rect.getHeight();
+
+		GL11.glMatrixMode(GL11.GL_PROJECTION);
+		GL11.glLoadIdentity();
+		GL11.glOrtho(0, w, h, 0, -1, 1);
+		GL11.glViewport(0, 0, w, h);
+	}
 	
 	private void updateGL() {
 		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
@@ -207,14 +203,7 @@ public class SwingFrame_LWJGL {
 		if (needUpdateViewport) {
 			needUpdateViewport = false;
 			
-			Rectangle rect = canvas.getBounds();
-			int w = (int) rect.getWidth();
-			int h = (int) rect.getHeight();
-			
-			GL11.glMatrixMode(GL11.GL_PROJECTION);
-			GL11.glLoadIdentity();
-			GL11.glOrtho(0, w, h, 0, -1, 1);
-			GL11.glViewport(0, 0, w, h);
+			makesize();
 		}
 		
 		int error = GL11.glGetError();
@@ -240,7 +229,7 @@ public class SwingFrame_LWJGL {
 	
 	private void render() {
 		float scale = 100;
-		
+		//System.out.println("render");
 		GL11.glBegin(GL11.GL_QUADS);
 		
 		GL11.glColor4f(1, 0, 0, 1);
