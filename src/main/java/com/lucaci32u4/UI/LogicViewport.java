2245392105cf278d931d223f44e6a2a1b8b37544
lucaci32u4 <lucaci32u4@gmail.com>
2018-10-07 16:56:13 +0300

    LWJGL drawing code, unfinished

--- src/main/java/com/lucaci32u4/UI/LogicViewport.java
+++ src/main/java/com/lucaci32u4/UI/LogicViewport.java
@@ -1,5 +1,12 @@
 package com.lucaci32u4.UI;
 
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.GL11;
+
+import javax.swing.*;
+import java.awt.*;
+
 public class LogicViewport {
 	private ViewportArtifact[] sprites;
 	private ViewportArtifact[] drawn;
@@ -7,6 +14,77 @@ public class LogicViewport {
 	private int pixelWidth, pixelHeight;
 	private int unitWidth, unitHeight;
 	private int unitOffsetX, unitOffsetY;
+	private float backgroudR, backgroundG, backgroundB;
+	private JPanel circuitPanel;
+	private Canvas canvas;
+	private RenderAPI pencil;
 
 
+	private void init() {
+		pencil = new GL11Backend();
+		canvas = new Canvas() {
+			@Override public void removeNotify() {
+				super.removeNotify();
+				pencil.destroyRenderer();
+			}
+
+			@Override
+			public void revalidate() {
+				super.revalidate();
+			}
+		};
+		circuitPanel.add(canvas);
+		pencil.initRenderer(canvas);
+	}
+
+	private static class GL11Backend implements RenderAPI {
+		private int width, height;
+		private float backgroundR, backgroundG, backgroundB;
+		private Canvas canvas;
+		@Override public void initRenderer(Canvas surface){
+			try {
+				Display.create();
+				Display.setParent(surface);
+				setBackgroundColor(1, 1, 1);
+				adjustToCanvasSize();
+				GL11.glClearColor(backgroundR, backgroundG, backgroundB, 1);
+			} catch (LWJGLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void render() {
+			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+
+			Display.update();
+			Display.sync(60);
+		}
+
+		@Override public void destroyRenderer () {
+			if (Display.isCreated()) {
+				Display.destroy();
+			}
+		}
+
+		@Override public void adjustToCanvasSize() {
+			Rectangle rect = canvas.getBounds();
+			width = (int)rect.getWidth();
+			height = (int)rect.getHeight();
+			GL11.glViewport(0, 0, width, height);
+			GL11.glMatrixMode(GL11.GL_PROJECTION);
+			GL11.glLoadIdentity();
+			GL11.glOrtho(0, width, height, 0, -1, 1);
+		}
+
+		@Override public void setBackgroundColor(float r, float g, float b) {
+			backgroundR = r; backgroundG = g; backgroundB = b;
+		}
+	}
+
+	private interface RenderAPI {
+		void initRenderer(Canvas surface);
+		void destroyRenderer();
+		void adjustToCanvasSize();
+		void setBackgroundColor(float r, float g, float b);
+	}
 }
