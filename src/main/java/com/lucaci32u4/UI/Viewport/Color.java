88e461ce0abb1c9577eff39e4978435a605d6d83
lucaci32u4 <lucaci32u4@gmail.com>
2018-10-08 19:29:08 +0300

    Created special colors instances in color class

--- src/main/java/com/lucaci32u4/UI/Viewport/Color.java
+++ src/main/java/com/lucaci32u4/UI/Viewport/Color.java
@@ -1,10 +1,12 @@
 package com.lucaci32u4.UI.Viewport;
 
 public class Color {
-	public static final int COLOR_LEVEL_TRISTATE = 0b00;
-	public final static int COLOR_LEVEL_LOW = 0b01;
-	public final static int COLOR_LEVEL_HIGH = 0b10;
-	public static final int COLOR_LEVEL_ERROR = COLOR_LEVEL_LOW | COLOR_LEVEL_HIGH | COLOR_LEVEL_TRISTATE;
+	public static Color BACKGROUND 	= new Color((short)256, (short)0, (short)0);
+	public static Color PATTERN 	= new Color((short)257, (short)0, (short)0);
+	public static Color TRISTATE 	= new Color((short)258, (short)0, (short)0);
+	public static Color LOW 		= new Color((short)259, (short)0, (short)0);
+	public static Color HIGH 		= new Color((short)260, (short)0, (short)0);
+	public static Color ERROR 		= new Color((short)261, (short)0, (short)0);
 	public short R, G, B;
 	public Color(short R, short G, short B) {
 		this.set(R, G, B);
