236acb7f0c81a5e2b1e14911c4d6bfb7643f61ac
lucaci32u4 <lucaci32u4@gmail.com>
2018-10-08 17:37:49 +0300

    Removed unused methods

--- src/main/java/com/lucaci32u4/UI/Viewport/ViewportArtifact.java
+++ src/main/java/com/lucaci32u4/UI/Viewport/ViewportArtifact.java
@@ -1,4 +1,4 @@
-package com.lucaci32u4.UI;
+package com.lucaci32u4.UI.Viewport;
 
 import com.lucaci32u4.UI.Viewport.LogicViewport;
 import org.jetbrains.annotations.NotNull;
@@ -6,7 +6,6 @@ import org.jetbrains.annotations.NotNull;
 public interface ViewportArtifact {
 	boolean isVisible();
 	boolean checkIfOnScreen(int screenLeft, int screenTop, int screenWidth, int screenHeight);
-	boolean checkIfOnPoint(int xPoint, int yPoint);
 	void onDraw(@NotNull LogicViewport.DrawAPI pen);
 	void setPickID(int ID);
 	int getPickID();
