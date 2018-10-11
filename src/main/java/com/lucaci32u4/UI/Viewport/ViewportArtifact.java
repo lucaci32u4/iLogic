24b4d0828421fe767249dd47dbd34a04e9be5c36
lucaci32u4 <lucaci32u4@gmail.com>
2018-10-11 23:50:56 +0300

    Added click event handler, added ResourceAPI parameter to drawing methods

--- src/main/java/com/lucaci32u4/UI/Viewport/ViewportArtifact.java
+++ src/main/java/com/lucaci32u4/UI/Viewport/ViewportArtifact.java
@@ -3,8 +3,9 @@ package com.lucaci32u4.UI.Viewport;
 import org.jetbrains.annotations.NotNull;
 
 public interface ViewportArtifact {
-	void onAttach();
 	boolean checkIfOnScreen(int screenLeft, int screenTop, int screenWidth, int screenHeight);
-	void onDraw(@NotNull LogicViewport.DrawAPI pen);
-	void onDetach();
+	boolean clickedOnScreen(int xUnits, int yUnits);
+	void onAttach(@NotNull LogicViewport.ResourceAPI resourceAPI);
+	void onDraw(@NotNull LogicViewport.DrawAPI pen, @NotNull LogicViewport.ResourceAPI resourceAPI);
+	void onDetach(@NotNull LogicViewport.ResourceAPI resourceAPI);
 }
