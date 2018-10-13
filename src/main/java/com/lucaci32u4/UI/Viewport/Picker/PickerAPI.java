f897ed6ff285d3f89eff43fab611fde2dbd1a4a9
lucaci32u4 <lucaci32u4@gmail.com>
2018-10-13 23:35:44 +0300

    Added picker/renderer manager interface

--- src/main/java/com/lucaci32u4/UI/Viewport/Picker/PickerAPI.java
+++ src/main/java/com/lucaci32u4/UI/Viewport/Picker/PickerAPI.java
@@ -1,11 +1,9 @@
 package com.lucaci32u4.UI.Viewport.Picker;
 
-public interface PickerAPI {
+public interface PickerAPI extends PickerManager {
 	int CHANNEL_EDITOR          = 0;
 	int CHANNEL_SIMULATION      = 1;
 	void init();
-	void attach(Hitbox hitbox, int pickerChannel);
-	void detach(Hitbox hitbox, int pickerChannel);
 	void pick(int pointerX, int pointerY, int pickerChannel);
 	void destroy();
 	class PickerEvent {
--- src/main/java/com/lucaci32u4/UI/Viewport/Renderer/RenderAPI.java
+++ src/main/java/com/lucaci32u4/UI/Viewport/Renderer/RenderAPI.java
@@ -6,7 +6,7 @@ import com.lucaci32u4.UI.Viewport.LogicViewport;
 import javax.swing.JPanel;
 import java.awt.Canvas;
 
-public interface RenderAPI extends DrawAPI, ResourceAPI {
+public interface RenderAPI extends DrawAPI, ResourceAPI, RenderManager {
 	void initRenderer(JPanel panel, LogicViewport viewport);
 	void destroyRenderer();
 	boolean requestRenderFrame(LogicViewport.ViewportData drawData);
