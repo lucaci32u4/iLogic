1b862e6d1ba6ca48418b800ea47d29659b0d69fb
lucaci32u4 <lucaci32u4@gmail.com>
2018-10-13 14:42:01 +0300

    Added picking classes and interfaces

---
+++ src/main/java/com/lucaci32u4/UI/Picker/Hitbox.java
@@ -0,0 +1,5 @@
+package com.lucaci32u4.UI.Picker;
+
+public class Hitbox {
+	public int left, top, right, bottom;
+}
---
+++ src/main/java/com/lucaci32u4/UI/Picker/HitboxManager.java
@@ -0,0 +1,5 @@
+package com.lucaci32u4.UI.Picker;
+
+public interface HitboxManager {
+
+}
