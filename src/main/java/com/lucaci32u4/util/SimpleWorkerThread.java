2f8f276908266440767109aeea8dfe26b5bae1c8
lucaci32u4 <lucaci32u4@gmail.com>
2018-10-13 19:41:35 +0300

    Added a simple worker thread class

--- src/main/java/com/lucaci32u4/util/SimpleWorkerThread.java
+++ src/main/java/com/lucaci32u4/util/SimpleWorkerThread.java
@@ -2,10 +2,37 @@ package com.lucaci32u4.util;
 
 public class SimpleWorkerThread {
 
-	private Runnable runnable;
+	private Thread thread;
+	private JSignal submit;
+	private Runnable task;
+	private boolean running;
 
-	public SimpleWorkerThread(Runnable runnable) {
-		this.runnable = runnable;
+	public SimpleWorkerThread(Runnable workerTask) {
+		task = workerTask;
+		submit = new JSignal(false);
+		thread = new Thread(() -> {
+			while (running) {
+				submit.waitFor(true);
+				submit.set(false);
+				if (running) task.run();
+				else break;
+			}
+		});
 	}
 
+	public void start() {
+		thread.start();
+	}
+
+	public void submit() {
+		submit.set(true);
+	}
+
+	public void exit(boolean join) {
+		running = false;
+		submit.set(true);
+		if (join) {
+			Helper.join(thread);
+		}
+	}
 }
