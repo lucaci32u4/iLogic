3bd473a1980770a52f1a5c4bcd9865a1c6ba42db
lucaci32u4 <lucaci32u4@gmail.com>
2018-10-13 15:49:39 +0300

    Added picking methods and picking channel

--- src/main/java/com/lucaci32u4/UI/Picker/HitboxManager.java
+++ src/main/java/com/lucaci32u4/UI/Picker/HitboxManager.java
@@ -1,5 +1,11 @@
 package com.lucaci32u4.UI.Picker;
 
 public interface HitboxManager {
-
+	int CHANNEL_EDITOR          = 0;
+	int CHANNEL_SIMULATION      = 1;
+	void attach(Hitbox hitbox, int pickerChannel);
+	void detach(Hitbox hitbox, int pickerChannel);
+	void pick(int pointerX, int pointerY, int pickerChannel);
+	void destroy();
 }
+
