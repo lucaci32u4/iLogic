74d6ef7a7571d44eada22b9c5f5223a331ad00b3
lucaci32u4 <lucaci32u4@gmail.com>
2018-10-13 15:49:01 +0300

    Added fields for parent pickable and two int's as user param

--- src/main/java/com/lucaci32u4/UI/Picker/Hitbox.java
+++ src/main/java/com/lucaci32u4/UI/Picker/Hitbox.java
@@ -2,4 +2,7 @@ package com.lucaci32u4.UI.Picker;
 
 public class Hitbox {
 	public int left, top, right, bottom, depth;
+	public Pickable parent;
+	public int userParam1;
+	public int userParam2;
 }
