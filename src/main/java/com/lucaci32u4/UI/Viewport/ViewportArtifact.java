71429b25b8b1345f016774a3123775c4d95ebea2
lucaci32u4 <lucaci32u4@gmail.com>
2018-10-13 14:40:08 +0300

    Removed unused picking method

--- src/main/java/com/lucaci32u4/UI/Viewport/ViewportArtifact.java
+++ src/main/java/com/lucaci32u4/UI/Viewport/ViewportArtifact.java
@@ -4,7 +4,6 @@ import org.jetbrains.annotations.NotNull;
 
 public interface ViewportArtifact {
 	boolean checkIfOnScreen(int screenLeft, int screenTop, int screenWidth, int screenHeight);
-	boolean clickedOnScreen(int xUnits, int yUnits);
 	void onAttach(@NotNull LogicViewport.ResourceAPI resourceAPI);
 	void onDraw(@NotNull LogicViewport.DrawAPI pen, @NotNull LogicViewport.ResourceAPI resourceAPI);
 	void onDetach(@NotNull LogicViewport.ResourceAPI resourceAPI);
