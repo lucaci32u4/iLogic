ad899e8323003f918a714de6ad9346ac8a108a02
lucaci32u4 <lucaci32u4@gmail.com>
2018-10-13 23:34:36 +0300

    Renamed parameter

--- src/main/java/com/lucaci32u4/UI/Viewport/Picker/PickerManager.java
+++ src/main/java/com/lucaci32u4/UI/Viewport/Picker/PickerManager.java
@@ -1,6 +1,6 @@
 package com.lucaci32u4.UI.Viewport.Picker;
 
 public interface PickerManager {
-	void attach(Hitbox hitbox, int channel);
-	void detach(Hitbox hitbox, int channel);
+	void attach(Hitbox hitbox, int pickerChannel);
+	void detach(Hitbox hitbox, int pickerChannel);
 }
