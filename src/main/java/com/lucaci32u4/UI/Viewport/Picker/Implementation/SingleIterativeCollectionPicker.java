c94d7f746a001e99b468eeefbcfd1d368fb5870d
lucaci32u4 <lucaci32u4@gmail.com>
2018-10-13 23:15:33 +0300

    Picker code

--- src/main/java/com/lucaci32u4/UI/Viewport/Picker/Implementation/SingleIterativeCollectionPicker.java
+++ src/main/java/com/lucaci32u4/UI/Viewport/Picker/Implementation/SingleIterativeCollectionPicker.java
@@ -2,36 +2,66 @@ package com.lucaci32u4.UI.Viewport.Picker.Implementation;
 
 import com.lucaci32u4.UI.Viewport.Picker.Hitbox;
 import com.lucaci32u4.UI.Viewport.Picker.PickerAPI;
+import com.lucaci32u4.util.SimpleEventQueue;
 
 import java.util.Collection;
 
 public abstract class SingleIterativeCollectionPicker implements PickerAPI {
 
+	private SimpleEventQueue<PickerAPI.PickerEvent> queue;
+
 	private Collection<Hitbox>[] channel;
+	private Thread thread;
 
 	public SingleIterativeCollectionPicker() {
+		queue = new SimpleEventQueue<>();
 		channel = createCollections(2);
+		thread = new Thread(this::run);
 	}
 
-	@Override public void attach(Hitbox hitbox, int pickerChannel) {
-		channel[pickerChannel].add(hitbox);
+	@Override
+	public void attach(Hitbox hitbox, int pickerChannel) {
+		queue.produce(new PickerEvent(PickerEvent.Type.ATTACH, hitbox, pickerChannel));
 	}
 
 	@Override public void detach(Hitbox hitbox, int pickerChannel) {
-		channel[pickerChannel].remove(hitbox);
+		queue.produce(new PickerEvent(PickerEvent.Type.DETACH, hitbox, pickerChannel));
 	}
 
 	@Override public void pick(int pointerX, int pointerY, int pickerChannel) {
-		Collection<Hitbox> ch = channel[pickerChannel];
-		for (Hitbox hitbox : ch) {
-			if (hitbox.left <= pointerX) if (pointerX <= hitbox.right) if (hitbox.top <= pointerY) if (pointerY <= hitbox.bottom) {
-				hitbox.parent.clicked(pointerX, pointerY, hitbox, pickerChannel);
+		synchronized (this) {
+			Collection<Hitbox> ch = channel[pickerChannel];
+			for (Hitbox hitbox : ch) {
+				if (hitbox.left <= pointerX) if (pointerX <= hitbox.right) if (hitbox.top <= pointerY) if (pointerY <= hitbox.bottom) {
+					hitbox.parent.clicked(pointerX, pointerY, hitbox, pickerChannel);
+				}
 			}
 		}
 	}
 
 	@Override public void destroy() {
+		queue.produce(new PickerEvent(PickerEvent.Type.EXIT, null, -1));
+	}
 
+	private void run() {
+		boolean running = true;
+		PickerAPI.PickerEvent event = null;
+		while (running) {
+			event = queue.consume(true);
+			synchronized (this) {
+				switch (event.type) {
+					case ATTACH:
+						channel[event.pickerChannel].add(event.hitbox);
+						break;
+					case DETACH:
+						channel[event.pickerChannel].remove(event.hitbox);
+						break;
+					case EXIT:
+						running = false;
+						break;
+				}
+			}
+		}
 	}
 
 	public abstract Collection<Hitbox>[] createCollections(int collectionCount);
--- src/main/java/com/lucaci32u4/UI/Viewport/Picker/PickerAPI.java
+++ src/main/java/com/lucaci32u4/UI/Viewport/Picker/PickerAPI.java
@@ -3,17 +3,23 @@ package com.lucaci32u4.UI.Viewport.Picker;
 public interface PickerAPI {
 	int CHANNEL_EDITOR          = 0;
 	int CHANNEL_SIMULATION      = 1;
+	void init();
 	void attach(Hitbox hitbox, int pickerChannel);
 	void detach(Hitbox hitbox, int pickerChannel);
 	void pick(int pointerX, int pointerY, int pickerChannel);
 	void destroy();
 	class PickerEvent {
 		public enum Type {
-			ATTACH, DETACH,
+			ATTACH, DETACH, EXIT,
 		}
-		Type type;
-		Hitbox hitbox;
-		int pickerChannel;
+		public Type type;
+		public Hitbox hitbox;
+		public int pickerChannel;
+		public PickerEvent(Type type, Hitbox hitbox, int pickerChannel) {
+			this.type = type;
+			this.hitbox = hitbox;
+			this.pickerChannel = pickerChannel;
+		}
 	}
 }
 
