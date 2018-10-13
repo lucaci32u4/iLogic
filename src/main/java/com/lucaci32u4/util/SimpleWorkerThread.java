aa1d023c4dd1056635e535a38cb9887607bb22e9
lucaci32u4 <lucaci32u4@gmail.com>
2018-10-13 18:17:10 +0300

    Started working on a simple worker thread class

---
+++ src/main/java/com/lucaci32u4/util/SimpleWorkerThread.java
@@ -0,0 +1,11 @@
+package com.lucaci32u4.util;
+
+public class SimpleWorkerThread {
+
+	private Runnable runnable;
+
+	public SimpleWorkerThread(Runnable runnable) {
+		this.runnable = runnable;
+	}
+
+}
