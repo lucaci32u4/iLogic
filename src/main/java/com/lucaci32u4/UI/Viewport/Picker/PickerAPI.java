3b08c8ead05ead59479a33f0dd7a8f664171a3e6
lucaci32u4 <lucaci32u4@gmail.com>
2018-10-14 23:02:43 +0300

    Moved constants

--- src/main/java/com/lucaci32u4/UI/Viewport/Picker/PickerAPI.java
+++ src/main/java/com/lucaci32u4/UI/Viewport/Picker/PickerAPI.java
@@ -1,8 +1,6 @@
 package com.lucaci32u4.UI.Viewport.Picker;
 
 public interface PickerAPI extends PickerManager {
-	int CHANNEL_EDITOR          = 0;
-	int CHANNEL_SIMULATION      = 1;
 	void init();
 	void pick(int pointerX, int pointerY, int pickerChannel);
 	void destroy();
--- src/main/java/com/lucaci32u4/UI/Viewport/Picker/PickerManager.java
+++ src/main/java/com/lucaci32u4/UI/Viewport/Picker/PickerManager.java
@@ -1,6 +1,8 @@
 package com.lucaci32u4.UI.Viewport.Picker;
 
 public interface PickerManager {
+	int CHANNEL_EDITOR          = 0;
+	int CHANNEL_SIMULATION      = 1;
 	void attach(Hitbox hitbox, int pickerChannel);
 	void detach(Hitbox hitbox, int pickerChannel);
 }
