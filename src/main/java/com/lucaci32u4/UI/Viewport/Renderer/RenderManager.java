531b507cf2d0650a7bcdc6a8dbb4ab966f051c59
lucaci32u4 <lucaci32u4@gmail.com>
2018-10-14 14:13:00 +0300

    Removed unused import stamenent

--- src/main/java/com/lucaci32u4/UI/Viewport/Renderer/RenderManager.java
+++ src/main/java/com/lucaci32u4/UI/Viewport/Renderer/RenderManager.java
@@ -1,7 +1,5 @@
 package com.lucaci32u4.UI.Viewport.Renderer;
 
-import com.lucaci32u4.UI.Viewport.Renderer.VisualArtifact;
-
 public interface RenderManager {
 	void attach(VisualArtifact sprite);
 	void detach(VisualArtifact sprite);
