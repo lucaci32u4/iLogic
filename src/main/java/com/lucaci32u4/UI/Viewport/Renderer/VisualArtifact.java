881210c22005706ded4564d3523af0d6fcac90fd
lucaci32u4 <lucaci32u4@gmail.com>
2018-10-19 21:22:18 +0300

    Added proper license notice in all files

--- src/main/java/com/lucaci32u4/UI/Viewport/Renderer/VisualArtifact.java
+++ src/main/java/com/lucaci32u4/UI/Viewport/Renderer/VisualArtifact.java
@@ -1,3 +1,32 @@
+/*
+ * iSignal - Digital circuit simulator
+ * Copyright (C) 2018-present Iercosan-Lucaci Alexandru
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ *    ||=============================================||
+ *    ||     _  _____  _          _            _     ||
+ *    ||    (_)/  ___|(_)       =)_)-         | |    ||
+ *    ||     _ \ `--.  _   __ _  _ __    __ _ | |    ||
+ *    ||    | | `--. \| | / _` || '_ \  / _` || |    ||
+ *    ||    | |/\__/ /| || (_| || | | || (_| || |    ||
+ *    ||    |_|\____/ |_| \__, ||_| |_| \__,_||_|    ||
+ *    ||                   __/ |                     ||
+ *    ||                  |___/  Digital Simulator   ||
+ *    ||=============================================||
+ */
+
 package com.lucaci32u4.UI.Viewport.Renderer;
 
 import org.jetbrains.annotations.NotNull;
