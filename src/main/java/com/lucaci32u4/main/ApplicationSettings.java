f201ea18ce7ca332f81c5d907cc2de42e2e8ddd1
lucaci32u4 <lucaci32u4@gmail.com>
2018-10-23 01:02:03 +0300

    Reformat code

--- src/main/java/com/lucaci32u4/main/ApplicationSettings.java
+++ src/main/java/com/lucaci32u4/main/ApplicationSettings.java
@@ -37,11 +37,8 @@ public class ApplicationSettings {
 	public static final String[] RENDERER_OPTIONS = { "j2d_def", "j2d_ogl", "j2d_d3d", "ogl_ogl" };
 	public static final String[] ANTIALIASING_OPTIONS = { "AA1", "AA2", "AA4", "AA8", "AA16" };
 	
-	
-	// Language
+	// State
 	public int language = 0;
-	
-	// Rendering
 	public int renderMethod = 0;
 	public int antialiasing = 0;
 }
