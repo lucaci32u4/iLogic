dea5b92ddb3d2b3b7555f20d4623cbdcc6211c69
lucaci32u4 <lucaci32u4@gmail.com>
2018-12-27 23:15:16 +0200

    Inserted inferred annotation and chnaged indentation fom spaces to tabs

--- src/main/java/com/lucaci32u4/core/Logic.java
+++ src/main/java/com/lucaci32u4/core/Logic.java
@@ -29,26 +29,28 @@
 
 package com.lucaci32u4.core;
 
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
 public class Logic {
-    public static final boolean LOW = false;
-    public static final boolean HIGH = true;
+	public static final boolean LOW = false;
+	public static final boolean HIGH = true;
 
-    public static final int FULL_DRIVER = 0b11;
-    public static final int HIGH_DRIVER = 0b01;
-    public static final int LOW_DRIVER = 0b10;
-    public static final int TRISTATE = 0b00;
+	public static final int FULL_DRIVER = 0b11;
+	public static final int HIGH_DRIVER = 0b01;
+	public static final int LOW_DRIVER = 0b10;
+	public static final int TRISTATE = 0b00;
 
-    public boolean state;
-    public boolean defined;
-    public Logic(boolean initialState, boolean define) { state = initialState; defined = define; }
-    public Logic(@NotNull Logic source) { state = source.state; defined = source.defined; }
-    public void copy(@NotNull Logic src) { state = src.state; defined = src.defined; }
-    @Override
-    public String toString() {
-    	return (defined ? (state ? "HIGH" : "LOW") : "TRISTATE");
-    }
+	public boolean state;
+	public boolean defined;
+	public Logic(boolean initialState, boolean define) { state = initialState; defined = define; }
+	public Logic(@NotNull Logic source) { state = source.state; defined = source.defined; }
+	public void copy(@NotNull Logic src) { state = src.state; defined = src.defined; }
+	@Override
+	public String toString() {
+		return (defined ? (state ? "HIGH" : "LOW") : "TRISTATE");
+	}
 
-    public static boolean senseChange(@NotNull Logic from, @NotNull Logic to) { return ((from.defined) && (from.state != to.state)) || (from.defined != to.defined); }
+	@Contract(pure = true)
+	public static boolean senseChange(@NotNull Logic from, @NotNull Logic to) { return ((from.defined) && (from.state != to.state)) || (from.defined != to.defined); }
 }
