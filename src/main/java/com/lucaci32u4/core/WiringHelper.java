92946926f0b927e4361736d0c360874bf7b1f1ed
lucaci32u4 <lucaci32u4@gmail.com>
2019-09-15 23:41:32 +0300

    Added a wiring helper

---
+++ src/main/java/com/lucaci32u4/core/WiringHelper.java
@@ -0,0 +1,91 @@
+package com.lucaci32u4.core;
+
+public class WiringHelper {
+	private boolean working = false;
+	private boolean hasDirection = false;
+	private boolean firstVertical = false;
+	private int beginX = 0;
+	private int beginY = 0;
+	private int endX = 0;
+	private int endY = 0;
+
+	public void startLink(int x, int y) {
+		if (working) throw new IllegalStateException("Cannot start a new link segment before another was finished");
+		else {
+			working = true;
+			hasDirection = false;
+			beginX = x;
+			beginY = y;
+			endX = x;
+			endY = y;
+		}
+	}
+
+	public void continueLink(int x, int y) {
+		if (!working) throw new IllegalStateException("Cannot continue a link while none was started");
+		endX = x;
+		endY = y;
+		if (!hasDirection && (beginX != endX || beginY != endY)) {
+			firstVertical = (beginX == endX);
+			hasDirection = true;
+		}
+	}
+
+	public void toggleDirection() {
+		firstVertical = !firstVertical;
+	}
+
+	/**
+	 * Get a preview of the links's coordinates
+	 * @param crdLocation an array of 6 integers that will be filled with data:
+	 *                    [0] first point X
+	 *                    [1] first point Y
+	 *                    [2] second point X
+	 *                    [3] second point Y
+	 *                    [4] third point X
+	 *                    [5] third point Y
+	 */
+	public void previewLinks(int[] crdLocation) {
+		if (crdLocation.length != 6) throw new IllegalArgumentException("Invalid array size. Expected 6, recieved " + crdLocation.length);
+		if (!working) throw new IllegalStateException("Cannot preview a link while none was started");
+		crdLocation[0] = beginX;
+		crdLocation[1] = beginY;
+		crdLocation[4] = endX;
+		crdLocation[5] = endY;
+		if (hasDirection) {
+			crdLocation[2] = (firstVertical ? beginX : endX);
+			crdLocation[3] = (firstVertical ? endY : beginY);
+		} else {
+			crdLocation[2] = endX;
+			crdLocation[3] = endY;
+		}
+	}
+
+	public CircuitLink[] endLink() {
+		if (!working) throw new IllegalStateException("Cannot end a link while none was started");
+		CircuitLink[] links = null;
+		if (beginX == endX && beginY == endY) {
+			links = new CircuitLink[0];
+		}
+		if (beginX == endX && beginY != endY) {
+			links = new CircuitLink[1];
+			links[0] = new CircuitLink(beginY, endY, false, beginX);
+		}
+		if (beginX != endX && beginY == endY) {
+			links = new CircuitLink[1];
+			links[0] = new CircuitLink(beginX, endX, true, beginY);
+		}
+		if (beginX != endX && beginY != endY) {
+			links = new CircuitLink[2];
+			if (firstVertical) {
+				links[0] = new CircuitLink(beginY, endY, true, beginX);
+				links[1] = new CircuitLink(beginX, endX, false, endY);
+			} else {
+				links[0] = new CircuitLink(beginX, endX, false, beginY);
+				links[1] = new CircuitLink(beginY, endY ,true, endX);
+			}
+		}
+		working = false;
+		return links;
+	}
+}
---
+++ src/test/java/com.lucaci32u4/core/WiringHelperTest.java
@@ -0,0 +1,39 @@
+package com.lucaci32u4.core;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WiringHelperTest {
+
+	@Test @Order(0)
+	void wiring() {
+		WiringHelper w = new WiringHelper();
+		int[] prev = new int[6];
+		CircuitLink[] links = null;
+		w.startLink(1, 2);
+		w.continueLink( 2, 2);
+		w.previewLinks(prev);
+		assertArrayEquals(new int[]{1, 2, 2, 2, 2, 2}, prev);
+		w.continueLink(4, 5);
+		w.previewLinks(prev);
+		assertArrayEquals(new int[]{1, 2, 4, 2, 4, 5}, prev);
+		links = w.endLink();
+		assertEquals(2, links.length);
+		assertEquals(new CircuitLink(1, 4, false, 2), links[0]);
+		assertEquals(new CircuitLink(2, 5, true, 4), links[1]);
+	}
+
+	@Test @Order(1)
+	void exceptions() {
+		WiringHelper w = new WiringHelper();
+		w.startLink(0, 0);
+		assertThrows(IllegalStateException.class, () -> w.startLink(2, 2));
+		w.endLink();
+		assertThrows(IllegalStateException.class, () -> w.endLink());
+		assertThrows(IllegalStateException.class, () -> w.continueLink(6, 6));
+		assertThrows(IllegalStateException.class, () -> w.previewLinks(new int[6]));
+		assertThrows(IllegalArgumentException.class, () -> w.previewLinks(new int[4]));
+	}
+}
